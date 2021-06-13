c639f803c89b90e2eadc321b16fd1e1570ddae4d
alekla0126 <alekla0126@gmail.com>
2021-06-13 15:15:14 -0500

    V3.36

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,22 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+    use Illuminate\Http\Request;
+    use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+    /*
+    |--------------------------------------------------------------------------
+    | API Routes
+    |--------------------------------------------------------------------------
+    |
+    | Here is where you can register API routes for your application. These
+    | routes are loaded by the RouteServiceProvider within a group which
+    | is assigned the "api" middleware group. Enjoy building your API!
+    |
+    */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+    Route::get('devices/{device_id}', 'DashboardController@get');
+
+    Route::middleware('auth:api')->group(function ()
+    {
+        Route::get('devices/{device_id}/save', 'DashboardController@store');
+    });
