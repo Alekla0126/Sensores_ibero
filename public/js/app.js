5756893ddaec7600063ee07db33b3284487773b2
alekla0126 <alekla0126@gmail.com>
2021-06-12 23:06:30 -0500

    V3.5

--- public/js/app.js
+++ public/js/app.js
@@ -37331,8 +37331,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/royvoetman/Desktop/Sites/iot-dashboard/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/royvoetman/Desktop/Sites/iot-dashboard/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/alejandro/PhpstormProjects/Sensores_ibero/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/alejandro/PhpstormProjects/Sensores_ibero/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
