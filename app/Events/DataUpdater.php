49c8ef08c3deb989f6a6f4b3231a40b51a3189f2
alekla0126 <alekla0126@gmail.com>
2021-06-13 02:47:03 -0500

    V3.24

--- app/Events/DataUpdater.php
+++ app/Events/DataUpdater.php
@@ -6,9 +6,9 @@ use App\Models\DeviceState;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 
-class DataUpdater implements ShouldBroadcast
+class DataUpdater implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
