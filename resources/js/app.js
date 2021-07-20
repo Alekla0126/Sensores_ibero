1c0b32937575bd255efd929cd84841b1312419a6
alekla0126 <alekla0126@gmail.com>
2021-07-19 20:40:03 -0500

    V5.15

--- resources/js/app.js
+++ resources/js/app.js
@@ -34,7 +34,6 @@ Vue.use(IconsPlugin)
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('table-devices', require('./components/TableDevices.vue').default);
-Vue.component('records-component', require('./components/Records.vue').default);
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
