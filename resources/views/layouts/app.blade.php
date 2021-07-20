e00f0efc7a300b7d38bf3f9a7c43f2cfdd043f60
alekla0126 <alekla0126@gmail.com>
2021-07-20 14:16:11 -0500

    V5.31

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-dark bg-warning shadow-lg">
+        <nav class="navbar navbar-expand-md navbar-dark bg-primary shadow-lg">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'IOT-Dashboard') }}
