9a47463d0d91b314dd57ae5f5c1a36b48715270b
alekla0126 <alekla0126@gmail.com>
2021-07-21 15:05:21 -0500

    V5.38

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -35,6 +35,9 @@ window.Pusher = require('pusher-js');
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: 'myKey',
-    forceTLS: false,
+    key: process.env.MIX_PUSHER_APP_KEY,
+    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+    forceTLS: true,
+    wsHost: window.location.hostname,
+    wsPort: 6001
 });
