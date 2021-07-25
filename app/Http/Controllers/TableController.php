cfc799ce74718e1d3a9e81a0938e3f01002ab65f
alekla0126 <alekla0126@gmail.com>
2021-07-24 23:14:49 -0500

    V5.45

--- app/Http/Controllers/TableController.php
+++ app/Http/Controllers/TableController.php
@@ -47,4 +47,5 @@
                 'devices' => $devices
             ]);
         }
+
     }
--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,7 +38,7 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'encrypted' => true,
-                'host' => 'http://143.198.164.212',
+                'host' => '143.198.164.212',
                 'port' => 6001,
                 'scheme' => 'http'
             ],
