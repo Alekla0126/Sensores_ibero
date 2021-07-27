ecde667da8b94584f11649f06d72bdd12499b701
alekla0126 <alekla0126@gmail.com>
2021-07-26 21:34:35 -0500

    V5.56 Configuring the bootstrap.js key manually.

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -35,12 +35,11 @@ window.Pusher = require('pusher-js');
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: process.env.MIX_PUSHER_APP_KEY,
+    key: 'myKey',
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
     encrypted: true,
     wsHost: window.location.hostname,
     wsPort: 6001,
-    wssPort: 6001,
     disableStats: true,
     forceTLS: false,
     enabledTransports: ['ws', 'wss']
