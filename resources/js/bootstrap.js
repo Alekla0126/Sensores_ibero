87f231b29fb5e9c4dcead8c5631e4f4e15d2a297
alekla0126 <alekla0126@gmail.com>
2021-08-04 23:27:09 -0500

    V7.18

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -33,6 +33,8 @@ import Echo from "laravel-echo"
 
 window.Pusher = require('pusher-js');
 
+Pusher.log = function (message) { window.console.log(message); };
+
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
