696483ce4c10355e4a2cab91df5225f352dd4854
alekla0126 <alekla0126@gmail.com>
2021-08-04 19:34:11 -0500

    V7.3

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,14 +37,10 @@
                 'app_id' => env('PUSHER_APP_ID'),
                 'options' => [
                     'cluster' => env('PUSHER_APP_CLUSTER'),
-                    'encrypted' => TRUE,
+                    'encrypted' => true,
+                    'host' => '127.0.0.1',
                     'port' => 6001,
-                    'scheme' => 'http',
-                    'useTLS' => false,
-                    'curl_options' => [
-                        CURLOPT_SSL_VERIFYHOST => 0,
-                        CURLOPT_SSL_VERIFYPEER => 0,
-                    ],
+                    'scheme' => 'http'
                 ],
             ],
 
--- config/websockets.php
+++ config/websockets.php
@@ -17,8 +17,8 @@
                 'name' => env('APP_NAME'),
                 'key' => env('PUSHER_APP_KEY'),
                 'secret' => env('PUSHER_APP_SECRET'),
-                'enable_client_messages' => FALSE,
-                'enable_statistics' => TRUE,
+                'enable_client_messages' => false,
+                'enable_statistics' => true,
             ],
         ],
 
