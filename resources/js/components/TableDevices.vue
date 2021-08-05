c16d37b3ce4a9838357526e172df58d248ee0997
alekla0126 <alekla0126@gmail.com>
2021-08-05 05:47:38 -0500

    V7.43 Forgot password conf

--- resources/js/components/TableDevices.vue
+++ resources/js/components/TableDevices.vue
@@ -76,97 +76,107 @@
 </template>
 
 <script>
-   export default {
-       props: ['devices'],
-       data()
-       {
-           return {
-               n_devices: [],
-               fields: [
-                   {
-                       label: 'Id',
-                       key: 'id',
-                       sortable: true
-                   },
-                   {
-                       label: 'Número del salón',
-                       key: 'device_id',
-                       sortable: true
-                   },
-                   {
-                       label: 'CO2',
-                       key: 'value',
-                       sortable: true,
-                   },
-                   {
-                       label: 'Creado en ',
-                       key: 'created_at',
-                       sortable: true,
-                   },
-                   {
-                       label: 'Actualizado en',
-                       key: 'updated_at',
-                       sortable: true,
-                   },
-               ],
-               items: this.devices,
-               filter: null,
-               filterOn: [],
-               currentPage: 1,
-               perPage: 10
-           }
-       },
-       created()
-       {
-           window.Echo.channel('table').listen('TableUpdater', (e) =>
-           {
-               for (let index = 0; index < e.message.length; index++)
-               {
-                   // Change the sensors limit.
-                   if(e.message[index]['value'] > 50)
-                   {
-                       e.message[index]['_rowVariant'] = 'danger';
-                   }
-               }
-               this.items = e.message;
-               this.updateTable();
-           });
-       },
-       mounted()
-       {
-           this.initializeTable(this.items);
-       },
-       methods: {
-           initializeTable(entry_data)
-           {
-               for (let index = 0; index < entry_data.length; index++)
-               {
-                   if(entry_data[index]['value'] > 50)
-                   {
-                       entry_data[index]['_rowVariant'] = 'danger';
-                   }
-               }
-               this.updateTable();
-           },
-           updateTable()
-           {
-               this.$refs.table.refresh();
-           },
-           onRowClick(record, index)
-           {
-               window.location.href = '/' + 'graph' + '/' + record['device_id'];
-           },
-           onFiltered(filteredItems)
-           {
-               this.totalRows = filteredItems.length;
-               this.currentPage = 1;
-           }
-       },
-       computed: {
-           rows()
-           {
-               return this.items.length
-           }
-       }
-   }
+import VueFlashMessage from '../src';
+
+Vue.use(VueFlashMessage);
+export default {
+    props: ['devices'],
+    data()
+    {
+        return {
+            n_devices: [],
+            fields: [
+                {
+                    label: 'Id',
+                    key: 'id',
+                    sortable: true
+                },
+                {
+                    label: 'Número del salón',
+                    key: 'device_id',
+                    sortable: true
+                },
+                {
+                    label: 'CO2',
+                    key: 'value',
+                    sortable: true,
+                },
+                {
+                    label: 'Creado en ',
+                    key: 'created_at',
+                    sortable: true,
+                },
+                {
+                    label: 'Actualizado en',
+                    key: 'updated_at',
+                    sortable: true,
+                },
+            ],
+            items: this.devices,
+            filter: null,
+            filterOn: [],
+            currentPage: 1,
+            perPage: 10
+        }
+    },
+    created()
+    {
+        window.Echo.channel('table').listen('TableUpdater', (e) =>
+        {
+            for (let index = 0; index < e.message.length; index++)
+            {
+                // Change the sensors limit.
+                if (e.message[index]['value'] > 50)
+                {
+                    e.message[index]['_rowVariant'] = 'danger';
+                    this.flash('Se excedieron los niveles en el salon: ' + e.message[index]['device_id'], 'warning', {
+                        timeout: 3000,
+                        beforeDestroy()
+                        {
+                            alert('Recuerda revisar el salon: ' + e.message[index]['device_id']);
+                        }
+                    });
+                }
+            }
+            this.items = e.message;
+            this.updateTable();
+        });
+    },
+    mounted()
+    {
+        this.initializeTable(this.items);
+    },
+    methods: {
+        initializeTable(entry_data)
+        {
+            for (let index = 0; index < entry_data.length; index++)
+            {
+                if (entry_data[index]['value'] > 50)
+                {
+                    entry_data[index]['_rowVariant'] = 'danger';
+                }
+            }
+            this.updateTable();
+        },
+        updateTable()
+        {
+            this.$refs.table.refresh();
+        },
+        onRowClick(record, index)
+        {
+            window.location.href = '/' + 'graph' + '/' + record['device_id'];
+        },
+        onFiltered(filteredItems)
+        {
+            this.totalRows = filteredItems.length;
+            this.currentPage = 1;
+        }
+    },
+    computed: {
+        rows()
+        {
+            return this.items.length
+        }
+    }
+}
 </script>
