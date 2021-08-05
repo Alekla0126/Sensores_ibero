5956501892613ca7c2931a21a0557c4f6355d0e1
alekla0126 <alekla0126@gmail.com>
2021-08-04 23:45:59 -0500

    V7.21

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,7 +37,7 @@
                 'app_id' => env('PUSHER_APP_ID'),
                 'options' => [
                     'cluster' => env('PUSHER_APP_CLUSTER'),
-                    'encrypted' => TRUE,
+                    'encrypted' => FALSE,
                     'host' => '127.0.0.1',
                     'port' => 6001,
                     'scheme' => 'http',
