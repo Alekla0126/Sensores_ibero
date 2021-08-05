b948448e70d655d61cb8f0a9129242fb642ad6a7
alekla0126 <alekla0126@gmail.com>
2021-08-04 20:51:54 -0500

    V7.9

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -31,16 +31,12 @@
         'connections' => [
 
             'pusher' => [
-                'driver' => 'pusher',
-                'key' => env('PUSHER_APP_KEY'),
-                'secret' => env('PUSHER_APP_SECRET'),
-                'app_id' => env('PUSHER_APP_ID'),
+                'driver'  => 'pusher',
+                'key'     => env('PUSHER_KEY'),
+                'secret'  => env('PUSHER_SECRET'),
+                'app_id'  => env('PUSHER_APP_ID'),
                 'options' => [
-                    'cluster' => env('PUSHER_APP_CLUSTER'),
-                    'encrypted' => true,
-                    'host' => '127.0.0.1',
-                    'port' => 6001,
-                    'scheme' => 'http'
+                    //leave these empty in your code
                 ],
             ],
 
