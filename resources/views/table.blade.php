3bb98f94a0e51604d190e9b168720951d4842e68
alekla0126 <alekla0126@gmail.com>
2021-08-06 17:43:49 -0500

    V7.66 Styling table

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -4,6 +4,7 @@
     <body>
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <div class="container-fluid">
+            <!--------------------Table component-------------------------->
             <table-devices v-bind:devices='@json($devices)'></table-devices>
         </div>
     </body>
