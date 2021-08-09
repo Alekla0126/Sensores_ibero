9ec482c69a5ab49afd0867f43991f192a2edb6c8
alekla0126 <alekla0126@gmail.com>
2021-08-08 21:39:22 -0500

    V8.57 The graph is updated

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -12,7 +12,7 @@
         public function index($device_id)
         {
             $blocks = DashboardBlock::query()->orderBy('order')->get();
-            $device = DeviceState::where('device_id', $device_id)->latest('updated_at')->take(7)->get()->reverse();
+            $device = DeviceState::where('device_id', $device_id)->latest('updated_at')->take(7)->get()->reverse()->values();
             return view('dashboard')->with([
                 'blocks',
                 $blocks,
