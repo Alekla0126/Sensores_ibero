1e04e464842f585ed50eb9c71fe2042294cadb71
alekla0126 <alekla0126@gmail.com>
2021-08-08 21:53:30 -0500

    V8.52 The graph is updated

--- resources/js/components/GraphicComponent.vue
+++ resources/js/components/GraphicComponent.vue
@@ -78,6 +78,9 @@ export default {
     {
         this.extractData();
         this.drawChart();
+        window.onresize = () => {
+            this.windowWidth = window.innerWidth
+        }
     },
     methods: {
         extractData()
